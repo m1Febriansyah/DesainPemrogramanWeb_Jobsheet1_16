4e6a79de3d976b46caf70a1de1178bf4000f0cf4
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-23 21:14:55 +0700

    Jobsheet4_Praktikum5_Soal5.4

--- P4/array.php
+++ P4/array.php
@@ -54,4 +54,28 @@ echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
 foreach ($daftarNilai[$mataKuliah] as $nilai) {
     echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
 }
+echo "<br>";
+$daftarNilai = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+];
+
+$totalNilai = 0;
+foreach ($daftarNilai as $siswa) {
+    $totalNilai += $siswa[1];
+}
+
+$rataRata = $totalNilai / count($daftarNilai);
+
+echo "Rata-rata nilai kelas: $rataRata <br><br>";
+echo "Daftar siswa dengan nilai di atas rata-rata:<br>";
+
+foreach ($daftarNilai as $siswa) {
+    if ($siswa[1] > $rataRata) {
+        echo "Nama: {$siswa[0]}, Nilai: {$siswa[1]} <br>";
+    }
+}
 ?>

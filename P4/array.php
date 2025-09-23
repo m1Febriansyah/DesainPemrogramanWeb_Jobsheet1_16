4047b1ac6d4e999e33cd82eb79ef7794d5f7ff36
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-23 20:57:47 +0700

    Jobsheet4_Praktikum5_Soal5.2

--- P4/array.php
+++ P4/array.php
@@ -8,6 +8,24 @@ foreach ($nilaiSiswa as $nilai) {
         $nilaiLulus[] = $nilai;
     }
 }
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+echo "<br>";
 
-echo "Daftar nilai siswa yang lulus: ". implode(", ", $nilaiLulus);
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+$karyawanPengalaman5Tahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " 
+    . implode(', ', $karyawanPengalamanLimaTahun);
 ?>

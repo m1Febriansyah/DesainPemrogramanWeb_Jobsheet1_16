c3beaaeeea92767353f4531ccfa9c1ce1f10f5ef
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-23 18:22:04 +0700

    Jobsheet4_Praktikum4_Soal4.4

--- P4/struktur_kontrol.php
+++ P4/struktur_kontrol.php
@@ -32,4 +32,14 @@ for($i = 1; $i <= $jumlahLahan; $i++){
     $jumlahBuah += $tanamanPerLahan * $buahPerTanaman;
 }
 echo "Jumlah buah yang akan dipanen adalah $jumlahBuah ";
+echo "<br>";
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach($skorUjian as $skor){
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah $totalSkor";
+echo "<br>";
 ?>

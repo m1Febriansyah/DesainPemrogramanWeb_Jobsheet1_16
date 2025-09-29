456bea52fc087d0a62bf6c5781647ab73be78a54
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-29 14:26:01 +0700

    Jobsheet5_PHP02_Soal2

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -1,10 +1,15 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya M.Febriansyah<br/>";
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
+perkenalan("Hamdana","Hallo");
 
-perkenalan();
-perkenalan();
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat Pagi";
+
+perkenalan($saya,$ucapanSalam);
 ?>

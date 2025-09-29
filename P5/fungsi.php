1c841c4eda5a161ec7e14443c44ed4c5787a9c1d
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-29 14:49:00 +0700

    Jobsheet5_PHP02_Soal4

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -1,15 +1,9 @@
 <?php
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+function hitungUmur($thn_lahir,$thn_sekarang){
+        $umur = $thn_sekarang - $thn_lahir;
+        return $umur;
 }
-perkenalan("Hamdana","Hallo");
 
-echo "<hr>";
+echo "Umur saya adalah ". hitungUmur(2007,2025). " tahun"
 
-$saya = "Elok";
-$ucapanSalam = "Selamat Pagi";
-
-perkenalan($saya);
 ?>

35d98b1a15befe061b8c24131377cc06a0e034a3
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-29 14:24:04 +0700

    Jobsheet5_PHP02_Soal1

---
+++ P5/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya M.Febriansyah<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+perkenalan();
+?>

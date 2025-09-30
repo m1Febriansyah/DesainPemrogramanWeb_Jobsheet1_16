1dd83f218534549538789a6e27fbece9f8c38d08
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-09-30 18:14:22 +0700

    Jobsheet5_PHP03_Soal1

---
+++ P5/JS05_PHP2/array_1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+ <h2>Array Terindeks dengan Loop</h2>
+ <?php
+$listDosen = ["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
+
+foreach($listDosen as $dosen){
+    echo $dosen . "<br>";
+}
+?>
+</body>
+</html>

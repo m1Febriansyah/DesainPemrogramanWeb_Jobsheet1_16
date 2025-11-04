e0392c879f4b62c5994a8d5846fd9a578bc26f10
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-11-04 20:06:12 +0700

    Jobsheet10.2_Praktikum1_Soal6.2

--- P10.2/ajax/index.php
+++ P10.2/ajax/index.php
@@ -68,6 +68,51 @@
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script type="text/javascript">
+        $("#simpan").click(function() {
+    var data = $('.form-data').serialize();
+    var jenkel1 = document.getElementById("jenkel1").value;
+    var jenkel2 = document.getElementById("jenkel2").value;
+    var nama = document.getElementById("nama").value;
+    var alamat = document.getElementById("alamat").value;
+    var no_telp = document.getElementById("no_telp").value;
+
+    if (nama == "") {
+        document.getElementById("err_nama").innerHTML = "Nama Harus Diisi";
+    } else {
+        document.getElementById("err_nama").innerHTML = "";
+    }
+    if (alamat == "") {
+        document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi";
+    } else {
+        document.getElementById("err_alamat").innerHTML = "";
+    }
+    if (document.getElementById("jenkel1").checked == false && document.getElementById("jenkel2").checked == false) {
+        document.getElementById("err_jenis_kelamin").innerHTML = "Jenis Kelamin Harus Dipilih";
+    } else {
+        document.getElementById("err_jenis_kelamin").innerHTML = "";
+    }
+    if (no_telp == "") {
+        document.getElementById("err_no_telp").innerHTML = "No Telepon Harus Diisi";
+    } else {
+        document.getElementById("err_no_telp").innerHTML = "";
+    }
+
+    if (nama != "" && alamat != "" && (document.getElementById("jenkel1").checked == true || document.getElementById("jenkel2").checked == true) && no_telp != "") {
+        $.ajax({
+            type: 'POST',
+            url: "form_action.php",
+            data: data,
+            success: function() {
+                $('#data').load("data.php");
+                document.getElementById("id").value = "";
+                document.getElementById("form-data").reset();
+            },
+            error: function(response) {
+                console.log(response.responseText);
+            }
+        });
+    }
+});
         $(document).ready(function() {
             $.ajaxSetup({
                 data: {
@@ -77,5 +122,41 @@
             $('#data').load('data.php');
         });
     </script>
+    <form method="post" class="form-data" id="form-data">
+    <div class="row">
+        <div class="col-sm-9">
+            <div class="form-group">
+                <label>Nama</label>
+                <input type="hidden" name="id" id="id">
+                <input type="text" name="nama" id="nama" class="form-control" required="true">
+                <p class="text-danger" id="err_nama"></p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="form-group">
+                <label>Jenis Kelamin</label><br>
+                <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true"> Laki-laki
+                <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+            </div>
+            <p class="text-danger" id="err_jenis_kelamin"></p>
+        </div>
+    </div>
+    <div class="form-group">
+        <label>Alamat</label>
+        <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
+        <p class="text-danger" id="err_alamat"></p>
+    </div>
+    <div class="form-group">
+        <label>No Telepon</label>
+        <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+        <p class="text-danger" id="err_no_telp"></p>
+    </div>
+    <div class="form-group">
+        <button type="button" name="simpan" id="simpan" class="btn btn-primary">
+            <i class="fa fa-save"></i> Simpan
+        </button>
+    </div>
+</form>
+
 </body>
 </html>
